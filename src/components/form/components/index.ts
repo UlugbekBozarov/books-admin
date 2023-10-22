7a86f06c11d8bdad1405ea7b4af92b944b730871
Ulug'bek Bozarov <ulugbek01081999@gmail.com>
2023-10-22 20:12:32 +0500

    updated

--- src/components/form/components/index.ts
+++ src/components/form/components/index.ts
@@ -1,8 +1,8 @@
 // export { default as Autocomplete } from "./Autocomplete/Autocomplete";
 // export { default as AutocompleteAddStaticOption } from "./AutocompleteAddStaticOption/AutocompleteAddStaticOption";
 // export { default as AutocompleteWithUrl } from "./AutocompleteWithUrl/AutocompleteWithUrl";
-export { default as Error } from "./errorCom/Error";
+export { default as Error } from "./error/Error";
 export { default as DisplayImage } from "./image/DisplayImage";
-export { default as Label } from "./labelCom/Label";
-export { default as NoData } from "./noDataCom/NoData";
+export { default as Label } from "./label/Label";
+export { default as NoData } from "./noData/NoData";
 export { default as ImageUploader } from "./upload/ImageUploader";
